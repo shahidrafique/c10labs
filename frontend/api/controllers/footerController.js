5d0e92af984c09c8247f713b5265bae330a631de
Abdullah Qureshi <111448185+arbimaq@users.noreply.github.com>
2024-02-28 11:59:33 +0500

    added console log in footer

--- frontend/api/controllers/footerController.js
+++ frontend/api/controllers/footerController.js
@@ -2,6 +2,12 @@ import strapiBuilder from "../builders/strapiBuilder";
 import attributesParser from "../parsers/attributesParser";
 import footer from "../queries/footer.gql";
 
-export const getFooter = strapiBuilder(footer.loc.source.body)("footer")(
+const _getFooter = strapiBuilder(footer.loc.source.body)("footer")(
   attributesParser
 );
+
+export const getFooter = async () => {
+  const data = await _getFooter();
+  console.log("footer data", data);
+  return data;
+};
