ede202bb73a568d538dec8323f9b2f5bacce808f
Shahid Rafique <shahid.rafique@arbisoft.com>
2024-03-06 15:22:59 +0500

    sponsors carousal

--- frontend/src/components/sponsors.jsx
+++ frontend/src/components/sponsors.jsx
@@ -12,28 +12,21 @@ export default function Sponsors({ logos }) {
         <h3>Perks Partners</h3>
       </div>
       <div className="logos-container">
-        <Slider
-          speed={5000}
-          dots={false}
-          pauseOnHover={false}
-          cssEase="linear"
-          slidesToScroll={1}
-          autoplay
-          autoplaySpeed={0}
-          variableWidth
-          arrows={false}
-        >
-          {/* duplicate to better fill up space on the screen */}
+        <div class="logos-slide">
           {logos?.map(({ logos: [{ url }] }) => (
             <StrapiImage key={url + 1} src={url} alt="sponsors" />
           ))}
+        </div>
+        <div class="logos-slide">
           {logos?.map(({ logos: [{ url }] }) => (
-            <StrapiImage key={url + 2} src={url} alt="sponsors" />
+            <StrapiImage key={url + 1} src={url} alt="sponsors" />
           ))}
+        </div>
+        <div class="logos-slide">
           {logos?.map(({ logos: [{ url }] }) => (
-            <StrapiImage key={url + 3} src={url} alt="sponsors" />
+            <StrapiImage key={url + 1} src={url} alt="sponsors" />
           ))}
-        </Slider>
+        </div>
       </div>
     </section>
   );
