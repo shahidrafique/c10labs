4c0699a3d52f4fd290059f45fa1900b6be5d546e
Abdullah Qureshi <111448185+arbimaq@users.noreply.github.com>
2024-03-07 16:11:03 +0500

    added tracking script

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -4,6 +4,7 @@ import "../../scss/styles.scss";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import SetupAnimations from "./setupAnimations";
+import Script from "next/script";
 
 export const metadata = {
   title: "C10 Labs",
@@ -25,6 +26,13 @@ export default function RootLayout({ children }) {
         {children}
         <Footer />
         <SetupAnimations />
+        <Script
+          type="text/javascript"
+          id="hs-script-loader"
+          async
+          defer
+          src="//js.hs-scripts.com/44606217.js"
+        />
       </body>
     </html>
   );
