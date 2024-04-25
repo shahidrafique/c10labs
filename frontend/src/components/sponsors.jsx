a50185412b4646afbd38f58eb20cbd58d93e147b
Abdullah Qureshi <111448185+arbimaq@users.noreply.github.com>
2024-04-25 14:22:32 +0500

    fixed structure

--- frontend/src/components/sponsors.jsx
+++ frontend/src/components/sponsors.jsx
@@ -4,29 +4,32 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import StrapiImage from "./StrapiImage";
+import { Fragment, useMemo } from "react";
 
 export default function Sponsors({ logos }) {
+  const nonEmptyLogos = useMemo(
+    () => logos?.filter(({ logos }) => logos.length > 0) ?? [],
+    [logos]
+  );
+
+  const Slide = (
+    <div className="logos-slide">
+      {nonEmptyLogos.map(({ logos: [{ url }] }) => (
+        <StrapiImage key={url + 1} src={url} alt="sponsors" />
+      ))}
+    </div>
+  );
+
   return (
     <section className="sponsors">
       <div className="title">
         <h3>Perks Partners</h3>
       </div>
       <div className="logos-container">
-        <div className="logos-slide">
-          {logos?.map(({ logos: [{ url }] }) => (
-            <StrapiImage key={url + 1} src={url} alt="sponsors" />
-          ))}
-        </div>
-        <div className="logos-slide">
-          {logos?.map(({ logos: [{ url }] }) => (
-            <StrapiImage key={url + 1} src={url} alt="sponsors" />
-          ))}
-        </div>
-        <div className="logos-slide">
-          {logos?.map(({ logos: [{ url }] }) => (
-            <StrapiImage key={url + 1} src={url} alt="sponsors" />
-          ))}
-        </div>
+        {/* multiple slides for animation */}
+        {new Array(3).fill(Slide).map((s, i) => (
+          <Fragment key={i}>{s}</Fragment>
+        ))}
       </div>
     </section>
   );
