e8c0693a4ef2c290ae86b3345b4d6ab629543808
Abdullah Qureshi <111448185+arbimaq@users.noreply.github.com>
2024-05-06 12:36:55 +0500

    added upcoming page

---
+++ frontend/src/app/upcoming/page.jsx
@@ -0,0 +1,13 @@
+export default function UpcomingPage() {
+  return (
+    <div className="container">
+      <iframe
+        className="airtable-embed"
+        src="https://airtable.com/embed/appUuJ8oXhsUBJSK7/shrqDqYtPa76K9tGT?backgroundColor=green&viewControls=on"
+        width="100%"
+        height="533"
+        style={{ background: "transparent", border: "1px solid #ccc" }}
+      />
+    </div>
+  );
+}
